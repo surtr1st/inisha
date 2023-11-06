5458eb6812a64eae4c04133ce4dd6102318dec3a
ishi <berugen1st@gmail.com>
2023-11-06 13:26:13 +0700

    add components config and extra colors

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,9 +1,22 @@
-export const colors = {
+const colors = {
    primary: '#5849ff',
    secondary: '#1E1E1E',
+   light: '#F1F1F1',
+   dark: '#1B1B1B',
 };
 
-export const hoverColors = {
+const hoverColors = {
    primary: '#7063ff',
    secondary: '#303030',
+   light: '#F9F9F9',
+   dark: '#242424',
+};
+
+export const components = {
+   background: {
+      ...colors,
+   },
+   hover: {
+      ...hoverColors,
+   },
 };
