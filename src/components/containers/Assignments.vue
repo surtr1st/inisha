61e84000115d80edd8995c7f1dd909bc6620e759
ishi <berugen1st@gmail.com>
2023-11-06 17:59:44 +0700

    handle overflow and background color for assignment

--- src/components/containers/Assignments.vue
+++ src/components/containers/Assignments.vue
@@ -12,10 +12,15 @@
 import { ref } from 'vue';
 import { TAssignments } from '../../types';
 import Chip from '../Chip.vue';
+import { components } from '../../configs';
 
 const { items } = defineProps<Partial<TAssignments>>();
 
 const chips = ref(items);
+const assignments = ref({
+   background: components.background.dark,
+   color: components.color.default,
+});
 
 const deleteSelectedChip = (index: number) => chips.value?.splice(index, 1);
 </script>
@@ -24,5 +29,14 @@ const deleteSelectedChip = (index: number) => chips.value?.splice(index, 1);
 .chips {
    display: flex;
    gap: 5px;
+   width: 100%;
+   height: 300px;
+   max-height: 350px;
+   overflow-x: hidden;
+   overflow-y: auto;
+   background: v-bind('assignments.background');
+   color: v-bind('assignments.color');
+   padding: 15px;
+   transition: all 250ms;
 }
 </style>
