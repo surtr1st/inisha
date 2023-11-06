b09225f4be1202b74fe31d407a94e33c4e5760dd
ishi <berugen1st@gmail.com>
2023-11-06 14:53:15 +0700

    add TModal and global types

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,4 +2,10 @@ export type TButton = {
    title: string;
    color: Color;
    disabled: boolean;
+   onClick: EventHandler;
+};
+export type TModal = {
+   open: boolean;
+   onClose: Getter<boolean>;
+   title: string;
 };
