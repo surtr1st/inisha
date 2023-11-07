0aadddb72c2e665cb9e88c70f256da775de22253
Ishi <berugen1st@gmail.com>
2023-11-08 06:16:08 +0700

    add TemplateSelection view

---
+++ src/views/TemplateSelection.vue
@@ -0,0 +1,91 @@
+<template>
+   <main>
+      <section class="template-header">
+         <h1>TECH SELECTION</h1>
+      </section>
+      <section class="template-body">
+         <div class="template-selection">
+            <Input
+               name="project-name"
+               placeholder="Name"
+               v-model="projectName" />
+         </div>
+         <div class="template-extra">
+            <Input
+               name="project-extra-deps"
+               placeholder="Dependency" />
+            <Assignments />
+         </div>
+         <div class="template-buttons">
+            <Button
+               title="Back"
+               color="secondary"
+               @click="replace('/')" />
+            <Button
+               title="Next"
+               color="primary"
+               @click="replace('/')" />
+         </div>
+      </section>
+   </main>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import Input from '../components/Input.vue';
+import Button from '../components/Button.vue';
+import Assignments from '../components/containers/Assignments.vue';
+
+const { replace } = useRouter();
+const projectName = ref('');
+</script>
+
+<style scoped>
+.template-header {
+   height: 7vh;
+   text-align: center;
+   margin: 2.1rem 0 0.5rem 0;
+}
+
+.template-header > h1 {
+   font-size: 48px;
+}
+
+.template-body {
+   height: 80vh;
+   margin: 2.1px;
+   overflow-x: hidden;
+   overflow-y: auto;
+   display: grid;
+   grid-template:
+      'selection selection selection extra extra extra'
+      'selection selection selection extra extra extra'
+      'selection selection selection extra extra extra'
+      'footer footer footer footer footer footer';
+   place-items: center;
+}
+
+.template-selection,
+.template-extra {
+   width: 30vw;
+}
+
+.template-selection {
+   grid-area: selection;
+}
+.template-extra {
+   grid-area: extra;
+}
+
+.template-buttons {
+   grid-area: footer;
+   width: 100%;
+   display: flex;
+   justify-content: flex-end;
+   align-items: center;
+   gap: 10px;
+   padding-right: 1rem;
+}
+</style>
+../services
