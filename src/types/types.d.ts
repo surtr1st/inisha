7b2ba049e3ef4b3e70618cc62562f9e0d892cd08
Ishi <berugen1st@gmail.com>
2023-11-08 06:26:47 +0700

    add MutableList type and update return type in projects"

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,3 +1,8 @@
 declare type Getter<T> = () => T | Promise<T>;
-declare type Setter<T> = (value: T) => void | Promise<void>
-declare type EventHandler = () => void | Promise<void>
+declare type Setter<T> = (value: T) => void | Promise<void>;
+declare type EventHandler = () => void | Promise<void>;
+declare type State<T> = [Getter<T>, Setter<T>];
+declare type MutableList<T1 extends Array<unknown>, T2 extends T1[0]> = [
+   Getter<T1>,
+   Setter<T2>,
+];
