f686b722d421dd73dccdcbcb966bf9598bb663e0
Ishi <berugen1st@gmail.com>
2023-11-08 11:21:24 +0700

    add PACKAGE_MANAGERS pre-defined data

--- src/data/integrations.ts
+++ src/data/integrations.ts
@@ -1,24 +1,47 @@
 import { TIntegration } from '../types';
 
 export const GLOBAL_INTEGRATIONS: TIntegration[] = [
-   {
-      alias: 'Use TypeScript',
-      value: 'typescript',
-      included: false,
-   },
-   {
-      alias: 'Integrate Prettier',
-      value: 'prettier',
-      included: false,
-   },
-   {
-      alias: 'Integrate ESLint',
-      value: 'eslint',
-      included: false,
-   },
-   {
-      alias: 'Integrate Tailwind',
-      value: 'tailwindcss',
-      included: false,
-   },
+     {
+          alias: 'Use TypeScript',
+          value: 'typescript',
+          included: false,
+     },
+     {
+          alias: 'Integrate Prettier',
+          value: 'prettier',
+          included: false,
+     },
+     {
+          alias: 'Integrate ESLint',
+          value: 'eslint',
+          included: false,
+     },
+     {
+          alias: 'Integrate Tailwind',
+          value: 'tailwindcss',
+          included: false,
+     },
+];
+
+export const PACKAGE_MANAGERS: TIntegration[] = [
+     {
+          alias: 'Use NPM',
+          value: 'npm',
+          included: false,
+     },
+     {
+          alias: 'Use Yarn',
+          value: 'yarn',
+          included: false,
+     },
+     {
+          alias: 'Use PNPM',
+          value: 'pnpm',
+          included: false,
+     },
+     {
+          alias: 'Use Bun',
+          value: 'bun',
+          included: false,
+     },
 ];
