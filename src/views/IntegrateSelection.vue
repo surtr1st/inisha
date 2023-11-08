0e6d483cc4ecb0e80dba20942605ed3f299534ee
Ishi <berugen1st@gmail.com>
2023-11-09 03:07:27 +0700

    use title and separate integrations

--- src/views/IntegrateSelection.vue
+++ src/views/IntegrateSelection.vue
@@ -5,11 +5,18 @@
           </section>
           <section class="template-body">
                <div class="template-selection">
-                    <Input
-                         name="project-name"
-                         placeholder="Name"
-                         v-model="projectName" />
-                    <div class="project-integrates">
+                    <Title
+                         class="project-name"
+                         value="Project Name"
+                         to="project-name">
+                         <Input
+                              name="project-name"
+                              placeholder="Name"
+                              v-model="projectName" />
+                    </Title>
+                    <Title
+                         value="Integrations"
+                         class="project-integrates">
                          <Switch
                               v-for="(integrate, index) in integrates"
                               :key="index"
@@ -17,13 +24,28 @@
                               :value="integrate.value"
                               :checked="integrate.included"
                               v-model="selectedIntegrate" />
-                    </div>
+                    </Title>
+                    <Title
+                         value="Package Managers"
+                         class="project-managers">
+                         <Switch
+                              v-for="(manager, index) in packageManagers"
+                              :key="index"
+                              :title="manager.alias"
+                              :value="manager.value"
+                              :checked="manager.included"
+                              v-model="selectedIntegrate" />
+                    </Title>
                </div>
                <div class="template-extra">
-                    <Input
-                         name="project-extra-deps"
-                         placeholder="Dependency" />
-                    <Assignments />
+                    <Title
+                         value="Extra Dependencies"
+                         to="project-extra-deps">
+                         <Input
+                              name="project-extra-deps"
+                              placeholder="Dependency" />
+                         <Assignments />
+                    </Title>
                </div>
                <div class="template-buttons">
                     <Button
@@ -42,29 +64,39 @@
 <script setup lang="ts">
 import { ref, watch } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
-import { GLOBAL_INTEGRATIONS, PACKAGE_MANAGERS } from '../data/index.js';
-import { useIntegrations } from '../services/index.js';
-import Input from '../components/Input.vue.js';
-import Button from '../components/Button.vue.js';
-import Assignments from '../components/containers/Assignments.vue.js';
-import Switch from '../components/Switch.vue.js';
+import { GLOBAL_INTEGRATIONS, PACKAGE_MANAGERS } from '../data';
+import { useIntegrations } from '../services';
+import Input from '../components/Input.vue';
+import Button from '../components/Button.vue';
+import Assignments from '../components/containers/Assignments.vue';
+import Switch from '../components/Switch.vue';
+import Title from '../components/Title.vue';
 
 const { replace } = useRouter();
 const { params } = useRoute();
-const [integrates, update] = useIntegrations(selectFromId());
+const [integrates, updateIntegrate] = useIntegrations(selectIntegratesFromId());
+const [packageManagers, updatePackageManager] = useIntegrations(
+     selectManagersFromId(),
+);
 
 const projectName = ref('');
 const selectedIntegrate = ref('');
 
-function selectFromId() {
+function selectIntegratesFromId() {
      const id = params.template;
      switch (id) {
           case '@vue':
-               return [...GLOBAL_INTEGRATIONS, ...PACKAGE_MANAGERS];
-          case '@react':
-               return [...GLOBAL_INTEGRATIONS, ...PACKAGE_MANAGERS];
-          case '@svelte':
-               return [...GLOBAL_INTEGRATIONS, ...PACKAGE_MANAGERS];
+               return GLOBAL_INTEGRATIONS;
+          default:
+               return GLOBAL_INTEGRATIONS;
+     }
+}
+
+function selectManagersFromId() {
+     const id = params.template;
+     switch (id) {
+          case '@vue':
+               return PACKAGE_MANAGERS;
           default:
                return PACKAGE_MANAGERS;
      }
@@ -75,53 +107,54 @@ watch(selectedIntegrate, () => {
      const index = integrates.value.findIndex(
           (integrate) => integrate.value === selectedIntegrate.value,
      );
-     update(index);
+     updateIntegrate(index);
      selectedIntegrate.value = '';
 });
 </script>
 
 <style scoped>
 .template-header {
-     height: 7vh;
+     height: 10vh;
      text-align: center;
      margin: 2.1rem 0 0.5rem 0;
 }
 
-.template-header > h1 {
-     font-size: 48px;
-}
-
 .template-body {
      height: 80vh;
-     margin: 2.1px;
+     margin: 0 2rem 0 2rem;
      overflow-x: hidden;
      overflow-y: auto;
      display: grid;
-     grid-template:
-          'selection selection selection extra extra extra'
-          'selection selection selection extra extra extra'
-          'selection selection selection extra extra extra'
-          'footer footer footer footer footer footer';
-     place-items: center;
+     grid-template-columns: repeat(4, 1fr);
+     column-gap: 10px;
+     grid-template-areas:
+          'ni ni extra extra'
+          'it pm extra extra'
+          'it pm extra extra'
+          'footer footer footer footer';
+}
+
+.project-name {
+     grid-area: ni;
 }
 
 .project-integrates {
-     width: 100%;
-     display: flex;
-     justify-content: space-around;
-     flex-direction: column;
+     grid-area: it;
 }
 
-.template-selection,
-.template-extra {
-     width: 30vw;
+.project-managers {
+     grid-area: pm;
 }
 
 .template-selection {
-     grid-area: selection;
+     grid-area: ni;
+     place-self: normal start;
+     width: 100%;
 }
 .template-extra {
      grid-area: extra;
+     width: 80%;
+     place-self: normal end;
 }
 
 .template-buttons {
@@ -131,7 +164,5 @@ watch(selectedIntegrate, () => {
      justify-content: flex-end;
      align-items: center;
      gap: 10px;
-     padding-right: 1rem;
 }
 </style>
-../services
